28140765de97c90a8ba26b7e7253a8c5c68db23c
kiarashfz <kiawfz3673@gmail.com>
2022-02-13 13:54:17 +0330

    gettext_lazy added for translate!

--- online_shop/core/models.py
+++ online_shop/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from .manager import BaseManager
+from django.utils.translation import gettext_lazy as _
 
 
 class BaseModel(models.Model):
