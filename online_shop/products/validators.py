540496b00c18e655a9291f1cefdcdfe62c40451f
kiarashfz <kiawfz3673@gmail.com>
2022-02-15 00:03:16 +0330

    validation func wrote for discount

--- online_shop/products/validators.py
+++ online_shop/products/validators.py
@@ -1,2 +1,7 @@
 from django.core.exceptions import ValidationError
+from products.models import Discount
 
+
+def discount_type_validator(discount: Discount):
+    if discount.type == 'percentage' and not 0 < discount.value < 100:
+        raise ValidationError('Percentage type value must be between 0 & 100.')
