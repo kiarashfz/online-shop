e9d18bcda7267feb435294fb3fedc8c36c1edac8
kiarashfz <kiawfz3673@gmail.com>
2022-02-14 06:38:06 +0330

    off code model and m2m model by customer created

--- online_shop/orders/models.py
+++ online_shop/orders/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from core.models import BaseModel
 from django.utils.translation import gettext_lazy as _
+from customers.models import Customer
 
 
 class BaseDiscount(BaseModel):
@@ -23,6 +24,46 @@ class BaseDiscount(BaseModel):
     )
 
     expire = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
+
+
+class Discount(BaseDiscount):
+    pass
+
+
+class CustomerOffCode(BaseModel):
+    customer = models.ForeignKey(
+        Customer,
+        on_delete=models.RESTRICT,
+    )
+
+    off_code = models.ForeignKey(
+        'OffCode',
+        on_delete=models.RESTRICT,
+    )
+
+    usable_count = models.PositiveSmallIntegerField(
+        null=True,
+        blank=True,
+        default=1,
+    )
+
+
+class OffCode(BaseDiscount):
+    code = models.CharField(
+        max_length=63,
+    )
+
+    min_buy = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+    )
+
+    customers = models.ManyToManyField(
+        Customer,
+        through=CustomerOffCode,
         null=True,
         blank=True,
     )
