680d51a141260d85e0ca7d61e2105ae549e594e4
kiarashfz <kiawfz3673@gmail.com>
2022-02-14 06:08:05 +0330

    BaseDiscount model created

--- online_shop/orders/models.py
+++ online_shop/orders/models.py
@@ -2,3 +2,27 @@ from django.db import models
 from core.models import BaseModel
 from django.utils.translation import gettext_lazy as _
 
+
+class BaseDiscount(BaseModel):
+    TYPES = [
+        ('percentage', _('Percentage')),
+        ('amount', _('Amount')),
+    ]
+
+    class Meta:
+        abstract = True
+
+    type = models.CharField(
+        max_length=15,
+        choices=TYPES,
+    )
+
+    max_amount = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+    )
+
+    expire = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
