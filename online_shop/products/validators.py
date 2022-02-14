45442b10c489a1e5994d405d12728fff6df10911
kiarashfz <kiawfz3673@gmail.com>
2022-02-15 00:04:58 +0330

    model changed to base dc

--- online_shop/products/validators.py
+++ online_shop/products/validators.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
-from products.models import Discount
+from products.models import BaseDiscount
 
 
-def discount_type_validator(discount: Discount):
+def discount_type_validator(discount: BaseDiscount):
     if discount.type == 'percentage' and not 0 < discount.value < 100:
         raise ValidationError('Percentage type value must be between 0 & 100.')
