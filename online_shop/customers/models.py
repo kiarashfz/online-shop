c35c0f8624c2684542ceec347af915841c66de29
kiarashfz <kiawfz3673@gmail.com>
2022-02-14 05:50:46 +0330

    address model created

--- online_shop/customers/models.py
+++ online_shop/customers/models.py
@@ -3,6 +3,91 @@ from core.models import BaseModel
 from django.utils.translation import gettext_lazy as _
 
 
-class Customer:
+class Customer(BaseModel):
     pass
 
+
+class Address(BaseModel):
+    PROVINCES = [
+        ('thr', _('Tehran')),
+        ('az-sh', _('Azarbayejan Sharghi')),
+        ('az-gh', _('Azarbayejan Gharbi')),
+        ('ard', _('Ardebil')),
+        ('esf', _('Esfehan')),
+        ('alb', _('Alborz')),
+        ('ilm', _('Ilam')),
+        ('bsh', _('Booshehr')),
+        ('chm', _('Charmahal o Bakhtiari')),
+        ('kh-j', _('Khorasan Jonobi')),
+        ('kh-r', _('Khorasan Razavi')),
+        ('kh-sh', _('Khorasan Shomali')),
+        ('khz', _('Khoozestan')),
+        ('znj', _('Zanjan')),
+        ('smn', _('Semnan')),
+        ('sbch', _('Sistan Baloochestan')),
+        ('frs', _('Fars')),
+        ('ghz', _('Ghazvin')),
+        ('qom', _('Qom')),
+        ('krd', _('Kordestan')),
+        ('krm', _('Kerman')),
+        ('kr-sh', _('Kerman Shah')),
+        ('khb', _('Kohkilooye Boyer Ahmad')),
+        ('gls', _('Golestan')),
+        ('gil', _('Gilan')),
+        ('lor', _('Lorestan')),
+        ('maz', _('Mazandaran')),
+        ('mrk', _('Markazi')),
+        ('hrm', _('Hormozgan')),
+        ('hmd', _('Hamedan')),
+        ('yzd', _('Yazd')),
+    ]
+
+    customer = models.ForeignKey(
+        Customer,
+        on_delete=models.RESTRICT,
+    )
+
+    province = models.CharField(
+        max_length=7,
+        choices=PROVINCES,
+    )
+
+    city = models.CharField(
+        max_length=31,
+    )
+
+    area = models.CharField(
+        max_length=31,
+    )
+
+    avenue = models.CharField(
+        max_length=31,
+    )
+
+    street = models.CharField(
+        max_length=31,
+        null=True,
+        blank=True,
+    )
+
+    alley = models.CharField(
+        max_length=31,
+        null=True,
+        blank=True,
+    )
+
+    no = models.PositiveSmallIntegerField()
+
+    unit = models.PositiveSmallIntegerField(
+        null=True,
+        blank=True,
+    )
+
+    floor = models.PositiveSmallIntegerField(
+        null=True,
+        blank=True,
+    )
+
+    postal_code = models.CharField(
+        max_length=31,
+    )
