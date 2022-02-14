e09e4df43c9cc001c2316660c24ecbf84c1aff42
kiarashfz <kiawfz3673@gmail.com>
2022-02-14 23:27:04 +0330

    product app test base models created!

--- online_shop/products/tests/tests_models.py
+++ online_shop/products/tests/tests_models.py
@@ -1,1 +1,33 @@
 from django.test import TestCase
+
+
+class DiscountTest(TestCase):
+    pass
+
+
+class OffCodeTest(TestCase):
+    pass
+
+
+class CustomerOffCodeTest(TestCase):
+    pass
+
+
+class CategoryTest(TestCase):
+    pass
+
+
+class BrandTest(TestCase):
+    pass
+
+
+class PropertyTest(TestCase):
+    pass
+
+
+class ProductTest(TestCase):
+    pass
+
+
+class CommentTest(TestCase):
+    pass
