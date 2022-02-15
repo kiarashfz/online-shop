1c65edf74c7bda53dff4f3cba11a58187e154b4b
kiarashfz <kiawfz3673@gmail.com>
2022-02-15 19:14:02 +0330

    validator func renamed and add gettext

--- online_shop/products/validators.py
+++ online_shop/products/validators.py
@@ -1,7 +1,8 @@
 from django.core.exceptions import ValidationError
 from products.models import BaseDiscount
+from django.utils.translation import gettext_lazy as _
 
 
-def discount_type_validator(discount: BaseDiscount):
+def validate_discount_type(discount: BaseDiscount):
     if discount.type == 'percentage' and not 0 < discount.value < 100:
-        raise ValidationError('Percentage type value must be between 0 & 100.')
+        raise ValidationError(_('Percentage type value must be between 0 & 100.'))
