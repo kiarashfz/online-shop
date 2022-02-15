25de4eb118c792ff24e47cced724c321e228a978
kiarashfz <kiawfz3673@gmail.com>
2022-02-15 19:37:14 +0330

    validator func moved to model full_clean func

--- online_shop/products/validators.py
+++ online_shop/products/validators.py
@@ -1,8 +1,2 @@
 from django.core.exceptions import ValidationError
-from products.models import BaseDiscount
 from django.utils.translation import gettext_lazy as _
-
-
-def validate_discount_type(discount: BaseDiscount):
-    if discount.type == 'percentage' and not 0 < discount.value < 100:
-        raise ValidationError(_('Percentage type value must be between 0 & 100.'))
