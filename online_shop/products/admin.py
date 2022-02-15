80c28898183f89ad3a02f40dc770f2839f8486c2
kiarashfz <kiawfz3673@gmail.com>
2022-02-15 22:05:25 +0330

    category & product added for admin register

--- online_shop/products/admin.py
+++ online_shop/products/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from products.models import Discount, OffCode
+from products.models import Discount, OffCode, Product, Category
 
 admin.site.register(Discount)
 admin.site.register(OffCode)
+admin.site.register(Product)
+admin.site.register(Category)
