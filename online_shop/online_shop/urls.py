1262702fd210f5cb91049b928328aeab35635f3e
kiarashfz <kiawfz3673@gmail.com>
2022-02-16 16:09:00 +0330

    media static url added to urls

--- online_shop/online_shop/urls.py
+++ online_shop/online_shop/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
@@ -23,4 +25,4 @@ urlpatterns = i18n_patterns(
     path('orders/', include('orders.urls')),
     path('products/', include('products.urls')),
     prefix_default_language=False,
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
