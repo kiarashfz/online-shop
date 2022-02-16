7345c585402f30cc1ef2c2c5da787e0698b3e5e0
kiarashfz <kiawfz3673@gmail.com>
2022-02-17 02:29:04 +0330

    test classes wrote but not completely

--- online_shop/orders/tests/tests_models.py
+++ online_shop/orders/tests/tests_models.py
@@ -12,18 +12,46 @@ class OrderTest(TestCase):
         self.category1 = Category.objects.create(name='Cars')
         self.discount2 = Discount.objects.create(type='percentage', value=20)
         self.product1 = Product.objects.create(name='Pride', price=10000, stock=7, category=self.category1,
-                                               discount=self.discount2)
-        self.off_code1 = OffCode.objects.create(code='ababab', usable_count=1, type='percentage', )
-        self.category2 = Category.objects.create(name='Cars2', discount=self.discount1)
-        self.address1 = Address.objects.create(province='thr', customer=self.customer1, city='tehran', area='pasdaran', avenue='dolat', no=5, postal_code='155662')
+                                               discount=self.discount1)
+        self.off_code1 = OffCode.objects.create(code='ababab', usable_count=1, type='percentage', value=20)
+        self.address1 = Address.objects.create(province='thr', customer=self.customer1, city='tehran', area='pasdaran'
+                                               , avenue='dolat', no=5, postal_code='155662')
         self.order1 = Order.objects.create(customer=self.customer1, address=self.address1, off_code=self.off_code1)
-        self.order_item1 = OrderItem.objects.create(product=self.product1, count=3, order=self.order1, customer=self.customer1)
-        self.product2 = Product.objects.create(name='z4', price=100000, stock=5, category=self.category2,
-                                               discount=self.discount2)
+        self.order_item1 = OrderItem.objects.create(product=self.product1, count=3, customer=self.customer1)
 
     def test_stock_reducer(self):
         self.order1.stock_reducer()
         self.assertEqual(self.product1.stock, 4)
 
+
+
+
+
+
+
+
     def test_off_code_check(self):
-        self
+        self.assertEqual(self.order1.off_code_check(), True)
+        self.assertEqual(self.order2.off_code_check(), False)
+        self.assertEqual(self.order3.off_code_check(), True)
+        self.assertEqual(self.order4.off_code_check(), True)
+        self.assertEqual(self.order5.off_code_check(), True)
+        self.assertEqual(self.order6.off_code_check(), False)
+
+
+# self.order2.stock_reducer()
+        # self.assertEqual(self.product1.stock, 4)self.order1.stock_reducer()
+        # self.assertEqual(self.product1.stock, 4)self.order1.stock_reducer()
+        # self.assertEqual(self.product1.stock, 4)
+
+
+# self.off_code2 = OffCode.objects.create(code='dhdh', usable_count=3, type='percentage', value=40)
+        # self.category2 = Category.objects.create(name='Cars2', discount=self.discount1)
+        # self.order2 = Order.objects.create(customer=self.customer1, address=self.address1, off_code=self.off_code1)
+        # self.order3 = Order.objects.create(customer=self.customer1, address=self.address1, off_code=self.off_code1)
+        # self.order4 = Order.objects.create(customer=self.customer1, address=self.address1, off_code=self.off_code2)
+        # self.order5 = Order.objects.create(customer=self.customer1, address=self.address1, off_code=self.off_code1)
+        # self.order6 = Order.objects.create(customer=self.customer1, address=self.address1, off_code=self.off_code1)
+        # self.order_item2 = OrderItem.objects.create(product=self.product1, count=3, order=self.order2, customer=self.customer1)
+        # self.product2 = Product.objects.create(name='z4', price=100000, stock=5, category=self.category2,
+        #                                        discount=self.discount2)
--- online_shop/products/tests/tests_models.py
+++ online_shop/products/tests/tests_models.py
@@ -1,33 +1,95 @@
 from django.test import TestCase
 
-
-class DiscountTest(TestCase):
-    pass
-
-
-class OffCodeTest(TestCase):
-    pass
-
-
-class CustomerOffCodeTest(TestCase):
-    pass
+from products.models import Discount, Category, Product, Brand
 
 
 class CategoryTest(TestCase):
-    pass
+    # todo: stock har dafe kam mishe ya na
+
+    def setUp(self) -> None:
+        self.category1 = Category.objects.create(name='Cars')
+        self.product1 = Product.objects.create(name='Pride', price=100000, stock=7, category=self.category1)
+        self.discount1 = Discount.objects.create(type='percentage', value=80)
+        self.discount2 = Discount.objects.create(type='percentage', value=10)
+        self.brand1 = Brand.objects.create(name='BMW')
+        self.product2 = Product.objects.create(name='X6', price=2000000000, stock=7, category=self.category1)
+
+    def tests_category_discount(self):
+        self.category1.discount = self.discount1
+        self.category1.save()
+        self.assertEqual(self.product1.final_price, 20000)
+        self.brand1.discount = self.discount2
+        self.brand1.save()
+        self.product2.brand = self.brand1
+        self.product2.save()
+        self.assertEqual(self.product2.final_price, 360000000)
+
+
+class CustomerOffCodeTest(TestCase):
+    def setUp(self) -> None:
+        pass
 
 
 class BrandTest(TestCase):
-    pass
+    def setUp(self) -> None:
+        self.category1 = Category.objects.create(name='Cars')
+        self.brand1 = Brand.objects.create(name='BMW')
+        self.product1 = Product.objects.create(name='Pride', price=10000, stock=7, category=self.category1,
+                                               brand=self.brand1)
+        self.discount1 = Discount.objects.create(type='percentage', value=80)
+
+    def tests_category_discount(self):
+        self.brand1.discount = self.discount1
+        self.brand1.save()
+        self.assertEqual(self.product1.brand.discount, self.discount1)
 
 
 class PropertyTest(TestCase):
-    pass
+    def setUp(self) -> None:
+        pass
 
 
 class ProductTest(TestCase):
-    pass
+    def setUp(self) -> None:
+        self.discount1 = Discount.objects.create(type='percentage', value=50)
+        self.category1 = Category.objects.create(name='Cars')
+        self.category2 = Category.objects.create(name='Cars2', discount=self.discount1)
+        self.discount2 = Discount.objects.create(type='percentage', value=20)
+        self.product1 = Product.objects.create(name='Pride', price=10000, stock=7, category=self.category1,
+                                               discount=self.discount2)
+        self.product2 = Product.objects.create(name='z4', price=100000, stock=5, category=self.category2,
+                                               discount=self.discount2)
+        self.product3 = Product.objects.create(name='z4', price=100000, stock=5, category=self.category2,
+                                               discount=self.discount2)
+        self.product4 = Product.objects.create(name='z4', price=100000, stock=5, category=self.category2,
+                                               discount=self.discount2)
+        self.product5 = Product.objects.create(name='z4', price=100000, stock=5, category=self.category2,
+                                               discount=self.discount2)
+        self.product6 = Product.objects.create(name='z4', price=100000, stock=5, category=self.category2,
+                                               discount=self.discount2)
+        self.product7 = Product.objects.create(name='z4', price=100000, stock=5, category=self.category2,
+                                               discount=self.discount2)
+
+    def test_final_price(self):
+        self.assertEqual(self.product1.final_price, 8000)
+        self.assertEqual(self.product2.final_price, 40000)
+        self.assertEqual(self.product3.final_price, 40000)
+        self.assertEqual(self.product4.final_price, 40000)
+        self.assertEqual(self.product5.final_price, 40000)
+        self.assertEqual(self.product6.final_price, 40000)
+        self.assertEqual(self.product7.final_price, 40000)
+
+
+class DiscountTest(TestCase):
+    def setUp(self) -> None:
+        self.discount1 = Discount.objects.create(type='percentage', value=200)
+
+
+class OffCodeTest(TestCase):
+    def setUp(self) -> None:
+        pass
 
 
 class CommentTest(TestCase):
-    pass
+    def setUp(self) -> None:
+        pass
