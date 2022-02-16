c6e43f307b2927b38672dd0d0aabc72689dc69f2
kiarashfz <kiawfz3673@gmail.com>
2022-02-16 17:18:16 +0330

    product admin wrote

--- online_shop/products/admin.py
+++ online_shop/products/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from products.models import Discount, OffCode, Product, Category, Brand, Property
 
+
+class ProductAdmin(admin.ModelAdmin):
+
+    def image_tag(self, obj):
+        return format_html('<img src="{}" width="50"/>'.format(obj.image.url))
+
+    image_tag.short_description = 'Image'
+
+    list_display = ['name', 'price', 'brand', 'stock', 'category', 'final_price', 'image']
+
+
 admin.site.register(Discount)
 admin.site.register(OffCode)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
 admin.site.register(Brand)
 admin.site.register(Property)
