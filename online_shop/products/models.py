f79d64ea168f1e748cea86042da02228537ff0e1
kiarashfz <kiawfz3673@gmail.com>
2022-02-16 07:04:18 +0330

    multiple discount for product wrote!

--- online_shop/products/models.py
+++ online_shop/products/models.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils import timezone
 from core.models import BaseModel
 from django.utils.translation import gettext_lazy as _
 from customers.models import Customer
@@ -53,6 +54,31 @@ class BaseDiscount(BaseModel):
 class Discount(BaseDiscount):
     class Meta:
         verbose_name = _('Discount')
+
+    def __add__(self, other):
+        if other is None:
+            return self
+        if self.type == 'percentage' and type(other) == Discount and other.type == 'percentage':
+            value = self.value + other.value - (self.value * other.value / 100)
+            max_amount = None
+            expire = None
+            if not self.max_amount and not other.max_amount:
+                max_amount = None
+            elif self.max_amount and other.max_amount:
+                max_amount = min(self.max_amount, other.max_amount)
+            elif not self.max_amount:
+                max_amount = other.max_amount
+            elif not other.max_amount:
+                max_amount = self.max_amount
+            if not self.expire and not other.expire:
+                expire = None
+            elif self.expire and other.expire:
+                expire = min(self.expire, other.expire)
+            elif not self.expire:
+                expire = other.expire
+            elif not other.expire:
+                expire = self.expire
+            return Discount(type='percentage', value=value, max_amount=max_amount, expire=expire)
 
     # todo: methode jaam vase discount vase zamani ke ham producte discount dare ham categorysh discount mikhore
 
@@ -124,6 +150,8 @@ class Category(BaseModel):
 
     name = models.CharField(
         max_length=31,
+        unique=True,
+        verbose_name=_('Name'),
     )
 
     parent = models.ForeignKey(
@@ -131,6 +159,8 @@ class Category(BaseModel):
         on_delete=models.RESTRICT,
         null=True,
         blank=True,
+        verbose_name=_('Parent'),
+        help_text=_('Parent category of this category!'),
     )
 
     discount = models.ForeignKey(
@@ -138,14 +168,30 @@ class Category(BaseModel):
         on_delete=models.RESTRICT,
         null=True,
         blank=True,
+        verbose_name=_('Discount'),
+        help_text=_('Discount for all of this category products!'),
+    )
+
+    def __str__(self):
+        return self.name
+
+    hidden_discount = models.ForeignKey(
+        Discount,
+        on_delete=models.RESTRICT,
+        null=True,
+        blank=True,
+        editable=False,
+        related_name='category_hidden_discount_set',
     )
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         super().save(force_insert, force_update, using, update_fields)
-        if self.discount:
-            for product in self.product_set:
-                product.discount = self.discount
+        if self.discount != self.hidden_discount:
+            for product in self.product_set.all():
+                product.category_discount = self.discount
                 product.save()
+            self.hidden_discount = self.discount
+            self.save()
     # todo: methodi k age discount barash set shod bere hame productaye toye in categoryo in discounto bzne
     # todo: age khode producte az qabl dc dasht dc e categoryo ba oon jaam kone
 
@@ -157,23 +203,23 @@ class Brand(BaseModel):
     name = models.CharField(
         max_length=31,
         unique=True,
-        verbose_name=_(''),
-        help_text=_(''),
+        verbose_name=_('Name'),
+        help_text=_('Name of the brand!'),
     )
 
     country = models.CharField(
         max_length=31,
         null=True,
         blank=True,
-        verbose_name=_(''),
-        help_text=_(''),
+        verbose_name=_('Country'),
+        help_text=_('Country of brand!'),
     )
 
     description = models.TextField(
         null=True,
         blank=True,
-        verbose_name=_(''),
-        help_text=_(''),
+        verbose_name=_('Description'),
+        help_text=_('Write description of brand!'),
     )
 
     discount = models.ForeignKey(
@@ -181,34 +227,51 @@ class Brand(BaseModel):
         on_delete=models.RESTRICT,
         null=True,
         blank=True,
-        verbose_name=_(''),
-        help_text=_(''),
+        verbose_name=_('Discount'),
+        help_text=_('Discount for all products of this brand!'),
     )
+
+    hidden_discount = models.ForeignKey(
+        Discount,
+        on_delete=models.RESTRICT,
+        null=True,
+        blank=True,
+        editable=False,
+        related_name='brand_hidden_discount_set',
+    )
+
+    def __str__(self):
+        return self.name
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         super().save(force_insert, force_update, using, update_fields)
-        if self.discount:
-            for product in self.product_set:
-                product.discount = self.discount
+        if self.discount != self.hidden_discount:
+            for product in self.product_set.all():
+                product.brand_discount = self.discount
                 product.save()
+            self.hidden_discount = self.discount
+            self.save()
     # todo: age khode producte az qabl dc dasht dc e categoryo ba oon jaam kone
 
 
 class Property(BaseModel):
     class Meta:
         verbose_name = _('Property')
+        unique_together = ('key', 'value')
 
     key = models.CharField(
         max_length=255,
-        verbose_name=_(''),
-        help_text=_(''),
+        verbose_name=_('Title'),
     )
 
     value = models.CharField(
         max_length=2555,
-        verbose_name=_(''),
-        help_text=_(''),
+        verbose_name=_('Value'),
+        help_text=_('The Value of this title!'),
     )
+
+    def __str__(self):
+        return self.key
 
 
 class Product(BaseModel):
@@ -217,8 +280,12 @@ class Product(BaseModel):
 
     name = models.CharField(
         max_length=31,
-        verbose_name=_(''),
-        help_text=_(''),
+        verbose_name=_('Name'),
+        unique=True,
+    )
+
+    price = models.PositiveIntegerField(
+        verbose_name=_('Price'),
     )
 
     brand = models.ForeignKey(
@@ -226,26 +293,26 @@ class Product(BaseModel):
         on_delete=models.RESTRICT,
         null=True,
         blank=True,
-        verbose_name=_(''),
-        help_text=_(''),
+        verbose_name=_('Brand'),
     )
 
     properties = models.ManyToManyField(
         Property,
-        verbose_name=_(''),
-        help_text=_(''),
+        verbose_name=_('Properties'),
+        help_text=_('The properties of product!'),
+        null=True,
+        blank=True,
     )
 
     stock = models.PositiveIntegerField(
-        verbose_name=_(''),
-        help_text=_(''),
+        verbose_name=_('Stock'),
+        help_text=_('Count of product that are available in store!'),
     )
 
     category = models.ForeignKey(
         Category,
         on_delete=models.RESTRICT,
-        verbose_name=_(''),
-        help_text=_(''),
+        verbose_name=_('Category'),
     )
 
     discount = models.ForeignKey(
@@ -253,9 +320,87 @@ class Product(BaseModel):
         on_delete=models.RESTRICT,
         null=True,
         blank=True,
-        verbose_name=_(''),
-        help_text=_(''),
+        verbose_name=_('Discount'),
+        help_text=_('Discount of this product!'),
     )
+
+    category_discount = models.ForeignKey(
+        Discount,
+        on_delete=models.RESTRICT,
+        related_name='category_discount_set',
+        editable=False,
+        null=True,
+        blank=True,
+        verbose_name=_('Category Discount'),
+        help_text=_('The discount of category')
+    )
+
+    brand_discount = models.ForeignKey(
+        Discount,
+        on_delete=models.RESTRICT,
+        related_name='brand_discount_set',
+        editable=False,
+        null=True,
+        blank=True,
+        verbose_name=_('Brand Discount'),
+        help_text=_('The discount of Brand')
+    )
+
+    discount_decrease = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+        editable=False,
+        verbose_name=_('Discount Decrease'),
+        default=0
+    )
+
+    category_discount_decrease = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+        editable=False,
+        verbose_name=_('Discount Decrease'),
+        default=0
+    )
+
+    brand_discount_decrease = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+        editable=False,
+        verbose_name=_('Discount Decrease'),
+        default=0
+    )
+
+    def __str__(self):
+        return f'{self.name}'
+
+    @property
+    def price_after_discounts(self):
+        if self.discount and self.discount.expire and self.discount.expire < timezone.now():
+            self.discount = None
+            self.save()
+        if self.category_discount and self.category_discount.expire and self.category_discount.expire < timezone.now():
+            self.category_discount = None
+            self.save()
+        if self.brand_discount and self.brand_discount.expire and self.brand_discount.expire < timezone.now():
+            self.brand_discount = None
+            self.save()
+        if not self.discount and not self.category_discount and not self.brand_discount:
+            return self.price
+        elif self.discount and (self.category_discount or self.brand_discount):
+            total_discount = self.discount + self.category_discount + self.brand_discount
+            return min(self.price - self.price * total_discount.value / 100, total_discount.max_amount) if total_discount.max_amount else self.price - self.price * total_discount.value / 100
+        elif self.category_discount and (self.brand_discount or self.discount):
+            total_discount = self.category_discount + self.brand_discount + self.discount
+            return min(self.price - self.price * total_discount.value / 100, total_discount.max_amount) if total_discount.max_amount else self.price - self.price * total_discount.value / 100
+        elif self.brand_discount:
+            total_discount = self.brand_discount
+            return min(self.price - self.price * total_discount.value / 100, total_discount.max_amount) if total_discount.max_amount else self.price - self.price * total_discount.value / 100
+        elif self.discount:
+            total_discount = self.discount
+            return min(self.price - self.price * total_discount.value / 100, total_discount.max_amount) if total_discount.max_amount else self.price - self.price * total_discount.value / 100
+        elif self.category_discount:
+            total_discount = self.category_discount
+            return min(self.price - self.price * total_discount.value / 100, total_discount.max_amount) if total_discount.max_amount else self.price - self.price * total_discount.value / 100
 
 
 class Comment(BaseModel):
