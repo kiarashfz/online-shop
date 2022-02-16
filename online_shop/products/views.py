b37d470f49cbaa8935010975c6a354279e595e1b
kiarashfz <kiawfz3673@gmail.com>
2022-02-16 17:09:56 +0330

    product list view added

--- online_shop/products/views.py
+++ online_shop/products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from products.models import Product
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/products_list.html'
