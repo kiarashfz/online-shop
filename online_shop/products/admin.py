5719764169f9c75637b70992ee345209a6d5b1e2
kiarashfz <kiawfz3673@gmail.com>
2022-02-16 07:00:42 +0330

    brand added for admin register

--- online_shop/products/admin.py
+++ online_shop/products/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from products.models import Discount, OffCode, Product, Category
+from products.models import Discount, OffCode, Product, Category, Brand
 
 admin.site.register(Discount)
 admin.site.register(OffCode)
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Brand)
