1d69101ddd530fd0597fa3e689a5a162e737a1ba
kiarashfz <kiawfz3673@gmail.com>
2022-02-16 16:08:17 +0330

    property added to admin register

--- online_shop/products/admin.py
+++ online_shop/products/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from products.models import Discount, OffCode, Product, Category, Brand
+from products.models import Discount, OffCode, Product, Category, Brand, Property
 
 admin.site.register(Discount)
 admin.site.register(OffCode)
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Brand)
+admin.site.register(Property)
