00cc8a3a0b0c2e237996e4aad1eea2aef71e5c86
kiarashfz <kiawfz3673@gmail.com>
2022-02-16 16:08:38 +0330

    media root configed

--- online_shop/online_shop/settings.py
+++ online_shop/online_shop/settings.py
@@ -132,3 +132,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
