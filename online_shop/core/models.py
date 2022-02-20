8fd5f34d4d48e692c0d4dd32fbffd6657a73a55d
kiarashfz <kiawfz3673@gmail.com>
2022-02-20 22:37:40 +0330

    unused imports deleted

--- online_shop/core/models.py
+++ online_shop/core/models.py
@@ -1,4 +1,4 @@
-from django.db import models, IntegrityError, transaction
+from django.db import models
 from django.utils import timezone
 from .manager import BaseManager
 from django.utils.translation import gettext_lazy as _
