1e6a25a4855d221e7d087e6c3fc146e8de50dee8
kiarashfz <kiawfz3673@gmail.com>
2022-02-20 22:41:53 +0330

    user model moved to core.models

--- online_shop/core/manager.py
+++ online_shop/core/manager.py
@@ -29,14 +29,3 @@ class MyUserManager(UserManager):
     def create_superuser(self, username=None, email=None, password=None, **extra_fields):
         username = extra_fields['phone']
         return super().create_superuser(username, email, password, **extra_fields)
-
-
-class User(AbstractUser):
-    objects = MyUserManager()
-
-    phone = models.CharField(
-        max_length=14,
-        unique=True,
-    )
-
-    USERNAME_FIELD = 'phone'
--- online_shop/core/models.py
+++ online_shop/core/models.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
-from .manager import BaseManager
+from .manager import BaseManager, MyUserManager
 from django.utils.translation import gettext_lazy as _
 
 
@@ -78,3 +79,14 @@ class BaseModel(models.Model):
     #             super().save(force_insert, force_update, using, update_fields)
     #     except IntegrityError:
     #         self.restore()
+
+
+class User(AbstractUser):
+    objects = MyUserManager()
+
+    phone = models.CharField(
+        max_length=14,
+        unique=True,
+    )
+
+    USERNAME_FIELD = 'phone'
