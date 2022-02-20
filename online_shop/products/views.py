cb57d0dd078375b4afed31d09391b0f36b79458a
kiarashfz <kiawfz3673@gmail.com>
2022-02-20 22:48:00 +0330

    api view function wrote and commented

--- online_shop/products/views.py
+++ online_shop/products/views.py
@@ -1,9 +1,31 @@
+from django.http import JsonResponse
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import ListView, TemplateView
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'products/products_list.html'
+    template_name = 'landing/html&css/html/pages/pricing.html'
+
+
+#
+# @csrf_exempt
+# def product_list_api(request):
+#     if request.method == 'GET':
+#         product_serializer = ProductSerializer(Product.objects.all(), many=True)
+#         return JsonResponse({'data': product_serializer.data}, status=200)
+#
+#     elif request.method == 'POST':
+#         data = request.POST
+#         product_serializer = ProductSerializer(data=data)
+#         if product_serializer.is_valid():
+#             new_product = product_serializer.save()
+#             return JsonResponse({'new_product_id': new_product.id}, status=201)
+#         else:
+#             return JsonResponse({'errors': product_serializer.errors}, status=400)
+#     else:
+#         return JsonResponse({}, status=405)
