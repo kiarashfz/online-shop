5b0d21f796e151df75ff5b039cda24f3a9dd9264
kiarashfz <kiawfz3673@gmail.com>
2022-02-21 07:17:20 +0330

    User serializer and api view added

--- online_shop/customers/apis.py
+++ online_shop/customers/apis.py
@@ -1,7 +1,13 @@
 from rest_framework import generics
 
+from core.models import User
 from customers.models import Customer
-from customers.serializers import CustomerSerializer
+from customers.serializers import CustomerSerializer, UserSerializer
+
+
+class UserCreateApiView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
 
 
 class CustomerCreateApiView(generics.CreateAPIView):
--- online_shop/customers/serializers.py
+++ online_shop/customers/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 
+from core.models import User
 from customers.models import Customer
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 
 class CustomerSerializer(serializers.ModelSerializer):
