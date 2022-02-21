90c96a25d4ff3af066378944e5c816fd3e411522
kiarashfz <kiawfz3673@gmail.com>
2022-02-21 06:28:10 +0330

    customer create api view added to urls

--- online_shop/customers/urls.py
+++ online_shop/customers/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from customers.apis import CustomerCreateApiView
+
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('customer_create_api/', CustomerCreateApiView.as_view(), name='customer_create_api'),
 ]
