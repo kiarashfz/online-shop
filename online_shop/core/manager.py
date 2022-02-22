fdcdc15c896e702ed8ff95886cd48c50d8a74a9e
kiarashfz <kiawfz3673@gmail.com>
2022-02-22 17:40:31 +0330

    core manager edit for creating regular user

--- online_shop/core/manager.py
+++ online_shop/core/manager.py
@@ -29,3 +29,7 @@ class MyUserManager(UserManager):
     def create_superuser(self, username=None, email=None, password=None, **extra_fields):
         username = extra_fields['phone']
         return super().create_superuser(username, email, password, **extra_fields)
+
+    def create_user(self, username=None, email=None, password=None, **extra_fields):
+        extra_fields['phone'] = username
+        return super().create_user(username, email, password, **extra_fields)
