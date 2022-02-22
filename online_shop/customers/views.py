6b19d7db21a3a80193fadb965f7268932647f381
kiarashfz <kiawfz3673@gmail.com>
2022-02-22 17:47:50 +0330

    customer create view wrote & about view added login required

--- online_shop/customers/views.py
+++ online_shop/customers/views.py
@@ -1,3 +1,27 @@
+from django.contrib.auth import login
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import TemplateView, FormView, CreateView
 
-# Create your views here.
+from core.models import User
+from customers.forms import UserForm
+from customers.models import Customer
+
+
+class CustomerCreateView(CreateView):
+    template_name = 'landing/html&css/html/pages/sign-up.html'
+    form_class = UserForm
+    success_url = reverse_lazy('products:products_list')
+
+    def form_valid(self, form):
+        response = super(CustomerCreateView, self).form_valid(form)
+        Customer.objects.create(user=self.object)
+        login(self.request, self.object)
+        return response
+
+
+class AboutTemplateView(LoginRequiredMixin, TemplateView):
+    template_name = 'landing/html&css/html/pages/about.html'
+
