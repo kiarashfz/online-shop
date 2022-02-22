d61f42151eabd6641acb29fd0d5c9a734b35b83d
kiarashfz <kiawfz3673@gmail.com>
2022-02-22 11:30:12 +0330

    UserAdmin and Base Admin created

--- online_shop/core/admin.py
+++ online_shop/core/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils import timezone
 
-# Register your models here.
+from core.models import User
+
+UserAdmin.list_display = ('phone', 'email', 'first_name', 'last_name', 'is_staff')
+UserAdmin.search_fields = ('phone', 'first_name', 'last_name', 'email')
+UserAdmin.ordering = ('phone',)
+UserAdmin.fieldsets[0][1]['fields'] = ('phone', 'password')
+admin.site.register(User, UserAdmin)
+
+
+class BaseAdmin(admin.ModelAdmin):
+    def logical_deleter(self, request, queryset):
+        queryset.update(deleted_at=timezone.now())
+        queryset.update(is_deleted=True)
+        queryset.update(is_active=False)
+
+    def deactivate(self, request, queryset):
+        queryset.update(is_active=False)
+
+    def activate(self, request, queryset):
+        queryset.update(is_active=True)
+
+    actions = ['logical_deleter', 'deactivate', 'activate']
