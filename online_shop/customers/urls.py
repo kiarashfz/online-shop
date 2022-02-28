17b60a13c3e0d53d9558626c76b65f671cdfeee6
kiarashfz <kiawfz3673@gmail.com>
2022-02-28 19:30:39 +0330

    user & address list & detail view added

--- online_shop/customers/urls.py
+++ online_shop/customers/urls.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordChangeView
 from django.urls import path
 
-from customers.apis import CustomerCreateApiView, UserCreateApiView
+from customers.apis import CustomerCreateApiView, UserCreateApiView, UserListApiView, UserDetailApiView, \
+    AddressListApiView, AddressDetailApiView
 from customers.views import CustomerCreateView, AboutTemplateView
 
 app_name = 'customers'
@@ -10,8 +11,12 @@ urlpatterns = [
     path('user_create_api/', UserCreateApiView.as_view(), name='user_create_api'),
     path('sign-up/', CustomerCreateView.as_view(), name='sign-up'),
     path('login/', LoginView.as_view(template_name='landing/html&css/html/pages/sign-in.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('about/', AboutTemplateView.as_view(), name='about'),
-    path('logout/', LogoutView.as_view(), {'next_page': 'landing/html&css/html/pages/pricing.html'}, name='logout'),
+    path('user_list_api_view/', UserListApiView.as_view(), name='user_list_api_view'),
+    path('user_detail_api_view/<int:pk>', UserDetailApiView.as_view(), name='user_detail_api_view'),
+    path('address_list_api_view/', AddressListApiView.as_view(), name='address_list_api_view'),
+    path('address_detail_api_view/<int:pk>', AddressDetailApiView.as_view(), name='address_detail_api_view'),
     # path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
     # path('password_change/', PasswordChangeView.as_view(), name='password_change'),
 ]
