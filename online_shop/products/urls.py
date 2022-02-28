b9e15a122ecbb926f17fe51bf08092ebf12fa594
kiarashfz <kiawfz3673@gmail.com>
2022-02-28 20:18:49 +0330

    category detail view and api view added

--- online_shop/products/urls.py
+++ online_shop/products/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from products.views import ProductListView, ProductDetailView
-from .aips import ProductListApiView, ProductDetailApiView
+from products.views import ProductListView, ProductDetailView, CategoryDetailView
+from .apis import ProductListApiView, ProductDetailApiView, CategoryProductsListApiView
 
 app_name = 'products'
 urlpatterns = [
@@ -10,5 +10,7 @@ urlpatterns = [
     # path('products_list_api/', product_list_api, name='products_list_api'),
     path('products_list_api/', ProductListApiView.as_view(), name='products_list_api'),
     path('products_detail_api/<int:pk>', ProductDetailApiView.as_view(), name='products_detail_api'),
+    path('category_detail/<int:pk>', CategoryDetailView.as_view(), name='category_detail'),
+    path('category_products_list_api/<int:category_id>', CategoryProductsListApiView.as_view(), name='category_products_list_api'),
     # path('products_tamplate/', ProductTemplateView.as_view(), name='products_template'),
 ]
