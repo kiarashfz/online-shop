8465c97e822ad9bd3ecb30a08858d0b3687b138f
kiarashfz <kiawfz3673@gmail.com>
2022-02-28 19:38:40 +0330

    order & orderitem serializers wrote

---
+++ online_shop/orders/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from orders.models import Order, OrderItem
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
