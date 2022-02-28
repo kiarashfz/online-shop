de86b22e35ef20abfe2bf7a88f1bab7b95433211
kiarashfz <kiawfz3673@gmail.com>
2022-02-28 19:39:22 +0330

    order & order item view set added by router

--- online_shop/orders/urls.py
+++ online_shop/orders/urls.py
@@ -1,5 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
+from orders.apis import OrderViewSet, OrderItemViewSet, OrderItemOfProduct
+from orders.views import NotLoginHandler
+
+router = DefaultRouter()
+router.register(r'orders', OrderViewSet)
+router.register(r'order_items', OrderItemViewSet)
+
+app_name = 'orders'
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('order_item_of_product', OrderItemOfProduct.as_view(), name='order_item_of_product'),
+    path('not_login_handler', NotLoginHandler.as_view(), name='not_login_handler'),
 ]
