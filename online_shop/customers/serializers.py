14d4847ac0a774c2429aa0a62f287bb64f7f3723
kiarashfz <kiawfz3673@gmail.com>
2022-02-28 19:35:49 +0330

    address serializer added

--- online_shop/customers/serializers.py
+++ online_shop/customers/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from core.models import User
-from customers.models import Customer
+from customers.models import Customer, Address
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -14,3 +14,11 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = '__all__'
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
+
+    customer = serializers.HyperlinkedRelatedField(view_name='customers:user_detail_api_view', read_only=True)
