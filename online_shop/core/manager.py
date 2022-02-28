ff2e936e2fe3051a6b2a357e1c2be96240ad3df8
kiarashfz <kiawfz3673@gmail.com>
2022-02-28 19:33:44 +0330

    username assigned by phone for users

--- online_shop/core/manager.py
+++ online_shop/core/manager.py
@@ -31,5 +31,5 @@ class MyUserManager(UserManager):
         return super().create_superuser(username, email, password, **extra_fields)
 
     def create_user(self, username=None, email=None, password=None, **extra_fields):
-        extra_fields['phone'] = username
+        username = extra_fields['phone']
         return super().create_user(username, email, password, **extra_fields)
