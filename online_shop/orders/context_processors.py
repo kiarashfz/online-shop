f26d905c8803c2ad0cfc7b797be1a289a588bc89
kiarashfz <kiawfz3673@gmail.com>
2022-02-28 19:37:11 +0330

    contex processor added for login & not login situations

---
+++ online_shop/orders/context_processors.py
@@ -0,0 +1,34 @@
+from core.models import User
+from customers.models import Customer
+from orders.models import OrderItem
+from products.models import Category
+
+
+def extras(request):
+    categories = Category.objects.all()
+    parent_categories = Category.objects.filter(parent=None)
+    if request.user.id:
+        user = request.user
+        customer = Customer.objects.get(user=user)
+        cart_count = OrderItem.objects.filter(customer=customer).count()
+        order_items_products_ids = customer.orderitem_set.all().values_list('product', flat=True)
+        order_items = customer.orderitem_set.all()
+        login = True
+    elif order_items := request.session.get('order_items', None):
+        user, customer = None, None
+        cart_count = len(order_items)
+        order_items_products_ids = order_items.keys()
+        order_items = order_items
+        login = False
+    else:
+        user, customer, cart_count, order_items_products_ids, order_items, login = None, None, 0, [], None, False
+    return {
+        'categories': categories,
+        'parent_categories': parent_categories,
+        'user': user,
+        'customer': customer,
+        'cart_count': cart_count,
+        'order_items_products_ids': order_items_products_ids,
+        'order_items': order_items,
+        'login': login,
+    }
