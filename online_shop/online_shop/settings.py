914139739c62c83c83d318f65312efdf49fc6c51
kiarashfz <kiawfz3673@gmail.com>
2022-03-02 12:55:11 +0330

    default paginate size changed to 3

--- online_shop/online_shop/settings.py
+++ online_shop/online_shop/settings.py
@@ -161,7 +161,7 @@ LOGOUT_REDIRECT_URL = 'products:products_list'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 3,
 }
 
 LOGGING = {
