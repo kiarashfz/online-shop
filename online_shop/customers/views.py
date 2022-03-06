68a754c0d0c616c42f7853749daa31e1f9da8021
kiarashfz <kiawfz3673@gmail.com>
2022-03-06 12:15:52 +0330

    create orderitem session after login or sign up handled

--- online_shop/customers/views.py
+++ online_shop/customers/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import View
@@ -8,6 +9,8 @@ from django.views.generic import TemplateView, FormView, CreateView
 from core.models import User
 from customers.forms import UserForm
 from customers.models import Customer
+from orders.models import OrderItem
+from products.models import Product
 
 
 class CustomerCreateView(CreateView):
@@ -19,9 +22,31 @@ class CustomerCreateView(CreateView):
         response = super(CustomerCreateView, self).form_valid(form)
         Customer.objects.create(user=self.object)
         login(self.request, self.object)
+        customer = Customer.objects.get(user_id=self.request.user.id)
+        if order_items := self.request.session.get('order_items', []):
+            for order_item in order_items:
+                product = Product.objects.get(pk=order_item)
+                OrderItem.objects.create(product=product, count=order_items[order_item], customer=customer)
         return response
 
 
 class AboutTemplateView(LoginRequiredMixin, TemplateView):
     template_name = 'landing/html&css/html/pages/about.html'
 
+
+class MyLoginView(LoginView):
+    template_name = 'landing/html&css/html/pages/sign-in.html'
+
+    def form_valid(self, form):
+        validated_form = super().form_valid(form)
+        customer = Customer.objects.get(user_id=self.request.user.id)
+        if order_items := self.request.session.get('order_items', []):
+            for order_item in order_items:
+                try:
+                    old_order_item = OrderItem.objects.get(customer=customer, product_id=order_item)
+                    old_order_item.count += order_items[order_item]
+                    old_order_item.save()
+                except OrderItem.DoesNotExist:
+                    product = Product.objects.get(pk=order_item)
+                    OrderItem.objects.create(product=product, count=order_items[order_item], customer=customer)
+        return validated_form
