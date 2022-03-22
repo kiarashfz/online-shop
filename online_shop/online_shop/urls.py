5c384fa3420061528ef9ee0cc79efb533b3bea37
kiarashfz <kiawfz3673@gmail.com>
2022-03-22 05:08:24 +0430

    captcha & azbank & chat added to urls

--- online_shop/online_shop/urls.py
+++ online_shop/online_shop/urls.py
@@ -13,21 +13,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from azbankgateways.urls import az_bank_gateways_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
+    path('bankgateways/', az_bank_gateways_urls()),
     path('rosetta/', include('rosetta.urls')),
     path('customers/', include('customers.urls')),
     path('orders/', include('orders.urls')),
     path('products/', include('products.urls')),
     path('contact/', include('contact.urls')),
+    path('chat/', include('chat.urls')),
     prefix_default_language=False,
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += [
+    path('captcha/', include('captcha.urls')),
+]
 
 admin.site.site_header = "Web Mall Admin"
 admin.site.site_title = "Web Mall"
