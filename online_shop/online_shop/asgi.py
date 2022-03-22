22642eaafa34a7df4c1ec11b1c1ae7f1ccaedab1
kiarashfz <kiawfz3673@gmail.com>
2022-03-22 05:04:46 +0430

    websocket added to protocoltypes

--- online_shop/online_shop/asgi.py
+++ online_shop/online_shop/asgi.py
@@ -7,10 +7,28 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
+# import os
+#
+# from django.core.asgi import get_asgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_shop.settings')
+#
+# application = get_asgi_application()
+
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_shop.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
