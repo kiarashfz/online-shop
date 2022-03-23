78d8bc108161c59a4bdb85d97940f7f49da37d12
kiarashfz <kiawfz3673@gmail.com>
2022-03-23 05:16:41 +0430

    comment create form wrote

--- online_shop/products/forms.py
+++ online_shop/products/forms.py
@@ -2,4 +2,10 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
+from products.models import Comment
 
+
+class CommentCreateForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = '__all__'
