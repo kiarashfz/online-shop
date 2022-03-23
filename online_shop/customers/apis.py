be56f57afce9367a814cd242104526ed9f774d37
kiarashfz <kiawfz3673@gmail.com>
2022-03-23 05:11:11 +0430

    kave negar package added for otp sign up with sms

--- online_shop/customers/apis.py
+++ online_shop/customers/apis.py
@@ -2,11 +2,18 @@ import logging
 
 from rest_framework import generics, authentication, renderers
 from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from core.models import User
 from customers.models import Customer, Address
 from customers.permissions import IsSuperUserPermission, IsOwnerPermission, IsAddressOwnerPermission
-from customers.serializers import CustomerSerializer, UserSerializer, AddressSerializer
+from customers.serializers import CustomerSerializer, UserSerializer, AddressSerializer, CommentSerializer
+
+import random
+from kavenegar import *
+
+from products.models import Comment
 
 
 class UserCreateApiView(generics.CreateAPIView):
@@ -109,3 +116,32 @@ class AddressUpdateApiView(generics.UpdateAPIView):
 
     def get_queryset(self):
         return Address.objects.filter(customer__user=self.request.user)
+
+
+class ConfirmCode(APIView):
+    def get(self, request):
+        confirm_code = random.randint(1000, 9999)
+        phone = request.GET.get('phone', None)
+        # try:
+        api = KavenegarAPI(
+            '63692F4778315056363552346E79796C55624C765973447649314859516156705A706557763430634D48513D')
+        params = {
+            'sender': '10008663',
+            'receptor': f'{phone}',
+            'message': f'Hi !\nWelcome to Web Mall!\n\nYour confirm code: {confirm_code}',
+        }
+        print(confirm_code)
+        # response = api.sms_send(params)
+        # except APIException as e:
+        #     print(e)
+        #     return Response(status=400)
+        # except HTTPException as e:
+        #     print(e)
+        #     return Response(status=400)
+        request.session['confirm_code'] = confirm_code
+        return Response(status=200)
+
+
+class CommentUpdateApiView(generics.UpdateAPIView):
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
