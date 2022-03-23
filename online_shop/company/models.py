978902626a571c3f1b9ab61d8b5d852beb4b482e
kiarashfz <kiawfz3673@gmail.com>
2022-03-23 05:07:35 +0430

    site setting model created with locationfield

--- online_shop/company/models.py
+++ online_shop/company/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from location_field.models.plain import PlainLocationField
 
-from core.models import BaseModel
+from core.models import BaseModel, SingletonBaseModel
 from django.utils.translation import gettext_lazy as _
 
 
@@ -17,3 +18,11 @@ class Question(BaseModel):
     answer = models.TextField(
         verbose_name=_('Answer'),
     )
+
+
+class SiteSetting(SingletonBaseModel):
+    site_name = models.CharField(max_length=100)
+    location = PlainLocationField(based_fields=['city'], zoom=7)
+
+    def __str__(self):
+        return self.site_name
